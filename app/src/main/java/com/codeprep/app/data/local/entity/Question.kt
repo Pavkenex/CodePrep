8c0ff4bac7dfdeac6784e120e011a18bab4f245e
Matija Pavić <42782711+Pavkenex@users.noreply.github.com>
2026-03-06 16:40:26 +0100

    feat: implemented login, course view and lesson view

---
+++ app/src/main/java/com/codeprep/app/data/local/entity/Question.kt
@@ -0,0 +1,10 @@
+package com.codeprep.app.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "questions")
+data class Question(@PrimaryKey val id: String, val text: String,
+               val options: List<String>, val correctIndex: Int, val explanation:String) {
+
+}
